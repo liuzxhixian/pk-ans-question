a9cd4d5ebadf31657df17442aa6ede86312b1bcd
刘志贤 <xiancreate@qq.com>
2022-08-09 10:04:44 +0800

    修改注释

--- src/main/java/com/xian/websocket/websocketdemo/constant/ClientStatus.java
+++ src/main/java/com/xian/websocket/websocketdemo/constant/ClientStatus.java
@@ -6,6 +6,9 @@ package com.xian.websocket.websocketdemo.constant;
  * @createTime 2021/10/24 15:35
  */
 public interface ClientStatus {
+    /**
+     * 等待状态
+     */
     String WAIT = "1";
 
     String READY = "2";
